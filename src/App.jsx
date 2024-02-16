90d79f0fecad852cd2c67fe259c91b8b215d40ed
m1ssy0u <varakorn2324@gmail.com>
2024-02-16 15:55:42 +0700

    fiximage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css"
 
 function App() {
   const [sayNo , setSayno] = useState(1)
-  const [gif , setGif] = useState("../willyou.gif")
+  const [gif , setGif] = useState("https://static.varakorn.net/willyou.gif")
   const [yesBtnSize , setYesBtnSize] = useState(100)
   const [text,setText] = useState("Will you be my valentine?")
   const [displayNoBtn , setDisplayNoBtn] = useState()
@@ -20,11 +20,11 @@ function App() {
         <button className="nobtn" style={{marginLeft : "15%",display : displayNoBtn,fontSize : "100%"}} onClick={()=>{
           setSayno(sayNo + 1)
           setYesBtnSize(yesBtnSize + 100)
-          if(sayNo >= 1 && sayNo < 4){
-            setGif("../3.gif")
+          if(sayNo >= 1 && sayNo < 3){
+            setGif("https://static.varakorn.net/sad.gif")
             setText("Answer yes, please.")
           }
-          else if(sayNo > 4){
+          else if(sayNo > 3){
             setDisplayNoBtn("none")
           }
         }}>No</button>
@@ -34,7 +34,7 @@ function App() {
   else if(sayNo == 0){
     return(
       <>
-          <img className="center" src="../love.gif" alt="" />
+          <img className="center" src="https://static.varakorn.net/love.gif" alt="" />
           <h2 className="center">Lov you naaa</h2>
       </>
     )
